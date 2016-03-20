9293899ff87401bc88b4df3f021c227ae7a12dcc
Giovanni Guarino <gguarino@teravisiongames.com>
2016-03-20 17:13:27 -0430

    Update

---
+++ Source/Assets/Scripts/Atributes/EnemyView.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyView : MonoBehaviour {
+
+    public Text enemyName;
+    public Slider HP;
+    public Slider AP;
+    public Image attackStateImage;
+    public Text attackStateText;
+    public Image moveStateImage;
+    public Text moveStateText;
+
+    public Collider2D colider;
+    public Collider2D sensor;
+    public Rigidbody2D rigid;
+
+    void Awake()
+    {
+        HP.minValue = 0;
+        AP.minValue = 0;
+    }
+
+	// Use this for initialization
+	void Init(string name, int maxHP, int maxAP) {
+        enemyName.text = name;
+        HP.maxValue = maxAP;
+        AP.maxValue = maxAP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Source/Assets/Scripts/GameData/Constants.cs
@@ -0,0 +1,65 @@
+﻿namespace Constants
+    {
+    public enum GENRE
+    {
+        MALE,
+        FEMALE
+    }
+
+    public enum ANATOMY
+    {
+        BIPED,
+        QUADRUPED
+    }
+
+    public enum ATTACKTYPE
+    {
+        RANGE,
+        MELEE,
+        KAMEKAZE
+    }
+
+    public enum THEME
+    {
+        FIRE,
+        ICE,
+        POISON,
+        MAGIC,
+        EARTH
+    }
+
+    public enum RARITY
+    {
+        COMMON,
+        RARE,
+        LEGENDARY
+    }
+
+    public enum ALLIANCE
+    {
+        FRIEND,
+        ENEMY
+    }
+
+    public enum GAMEELEMENT
+    {
+        BASE,
+        TOWER,
+        WARRIOR,
+        MINION,
+        SUMMONED
+    }
+
+    public enum GAMEPLAYRESULT
+    {
+        NONE,
+        TIME,
+        WON,
+        LOSE,
+        SURRENDER,
+        DRAW
+
+    }
+
+}
+
